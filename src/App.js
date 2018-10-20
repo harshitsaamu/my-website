73131a1400c15c74832521199e68ede176b06365
Harshit Gupta <harshitsaamu@gmail.com>
2018-10-20 17:36:39 +0530

    fetching pin code data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Forms from "./components/Forms";
 import Weather from "./components/Weather";
 import './App.css';
 
-const API_KEY = "";
+const API_KEY = "AIzaSyBRxeLwZbCtK7OX45szIqaV_JY_pErMdrQ";
 
 class App extends Component {
+    getLocation = async(e) => {
+        e.preventDefault();
+        let pinCode = document.getElementById("pincode");
+        const apiCall = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${pinCode}&key=${API_KEY}`);
+        const data = await apiCall.json();
+        console.log(data);
+    };
+
     render() {
         return (
             <div>
                 <Titles/>
-                <Forms/>
+                <Forms getLocation={this.getLocation}/>
                 <Weather/>
             </div>
         );
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -4,7 +4,7 @@ class Forms extends React.Component {
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.props.getLocation}>
                     <input type={"text"} name={"pincode"} placeholder={"Pin Code"}/>
                     <br/>
                     <button type={"submit"} className={"button"}>Extract Data</button>
