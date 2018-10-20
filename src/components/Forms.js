2f92bc165d1918a5659961be6e85eefa12c4de2c
Harshit Gupta <harshitsaamu@gmail.com>
2018-10-20 07:56:12 +0530

    inital commit

---
+++ src/components/Forms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Forms extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type={"text"} name={"pincode"} placeholder={"Pin Code"}/>
+                    <br/>
+                    <button type={"submit"} className={"button"}>Extract Data</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Forms;
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render(){
+        return(
+           <div>
+               <h1>Let's checkout React</h1>
+               <p>It seems to be a good platform to build applications</p>
+           </div>
+        );
+    }
+}
+
+export default Titles;
