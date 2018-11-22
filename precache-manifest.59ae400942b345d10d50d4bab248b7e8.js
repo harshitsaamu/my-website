fe75f4b464399da660bbb0f58bee70ad2e31686c
Harshit Gupta <harshitsaamu@gmail.com>
2018-11-22 09:37:24 +0530

    Updates

---
+++ precache-manifest.59ae400942b345d10d50d4bab248b7e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "f1f51f5343a1071708e0",
+    "url": "/my-app/static/js/main.f1f51f53.chunk.js"
+  },
+  {
+    "revision": "f15db6f655ebf1ba5c26",
+    "url": "/my-app/static/js/1.f15db6f6.chunk.js"
+  },
+  {
+    "revision": "f1f51f5343a1071708e0",
+    "url": "/my-app/static/css/main.eba3b6a2.chunk.css"
+  },
+  {
+    "revision": "a08787549a3db6179b1c04b814fd76e8",
+    "url": "/my-app/index.html"
+  }
+];
